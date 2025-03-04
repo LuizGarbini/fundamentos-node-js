f77b6109da5a2e7ac4374970bfb0f76b09d1ae4d
LuizGarbini <garbasneto@gmail.com>
2025-03-04 00:47:28 -0300

    URL da requisição

--- src/server.js
+++ src/server.js
@@ -2,9 +2,17 @@ import http from "node:http";
 
 const server = http.createServer((request, response) => {
   // Desestruturação do method
-  const { method } = request;
+  const { method, url } = request;
 
-  return response.writeHead(200).end("Método: " + method);
+  if(method === "GET" && url === "/products") {
+    return response.end("Lista de produtos!")
+  }
+
+  if(method === "POST" && url === "/products") {
+    return response.writeHead(201).end("Produto cadastrado!")
+  }
+
+  return response.writeHead(404).end("Rota não encontrada");
 });
 
 /* Aqui a função fica escutando o servidor */
