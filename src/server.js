04a85bed69a90eea1f4be81dd0da8c53f0c48da9
LuizGarbini <garbasneto@gmail.com>
2025-03-04 00:38:45 -0300

    Identificando o método HTTP

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import http from "node:http";
 
 const server = http.createServer((request, response) => {
-  return response.end("Minha primeira API!")
+  // Desestruturação do method
+  const { method } = request;
+
+  return response.end("Método: " + method);
 });
 
-
 /* Aqui a função fica escutando o servidor */
-server.listen(3333)
+server.listen(3333);
