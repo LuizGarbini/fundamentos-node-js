6481bf9c0825cf3eb7398e5bf8c1b00d2977d37c
LuizGarbini <garbasneto@gmail.com>
2025-03-04 23:11:49 -0300

    Separando as rotas

--- src/server.js
+++ src/server.js
@@ -1,25 +1,10 @@
 import http from "node:http";
 import { jsonBodyHandler } from "./middlewares/jsonBodyHandler.js";
+import { routeHandler } from "./middlewares/routeHandler.js";
 
 const server = http.createServer(async (request, response) => {
-  // Desestruturação do method
-  const { method, url } = request;
-
-  await jsonBodyHandler(request, response)
-
-  // Definindo condições para o método GET
-  if (method === "GET" && url === "/products") {
-    return response.end("Lista de produtos!");
-  }
-
-  // Definindo condições para o método POST
-  if (method === "POST" && url === "/products") {
-    console.log(request.body)
-    return response.writeHead(201).end(JSON.stringify(request.body));
-  }
-
-  // Caso não entre em nenhum dos ifs acima ele da como rota não encontrada.
-  return response.writeHead(404).end("Rota não encontrada");
+  await jsonBodyHandler(request, response);
+  routeHandler(request, response);
 });
 
 /* Aqui a função fica escutando o servidor */
